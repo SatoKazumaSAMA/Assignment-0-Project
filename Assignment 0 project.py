90a7789580f763e17a65657460ee4a18dd928bcf
Chuyang Wang <143976558+SatoKazumaSAMA@users.noreply.github.com>
2024-09-18 00:58:22 -0500

    Add files via upload

---
+++ Assignment 0 project.py
@@ -0,0 +1,9 @@
+products={"Phone":5800,"Coffee":30, "Watch":6800,"Pen": 20}
+
+totalprice = products["Phone"] + products["Coffee"] + products["Watch"] + products["Pen"]
+
+sale = (totalprice // 400) * 50
+
+Total = totalprice - sale
+
+print(Total)
